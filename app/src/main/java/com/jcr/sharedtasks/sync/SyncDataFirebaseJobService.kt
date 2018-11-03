af511e697f8298d7f618c04a1b8071c125e60f3c
JaumeCasafont <jaucr94@gmail.com>
2018-11-03 13:34:36 +0100

    job service to Kotlin

---
+++ app/src/main/java/com/jcr/sharedtasks/sync/SyncDataFirebaseJobService.kt
@@ -0,0 +1,35 @@
+package com.jcr.sharedtasks.sync
+
+import com.firebase.jobdispatcher.JobParameters
+import com.firebase.jobdispatcher.JobService
+import com.jcr.sharedtasks.model.Task
+import com.jcr.sharedtasks.repository.ProjectsRepository
+
+import javax.inject.Inject
+
+import dagger.android.AndroidInjection
+
+class SyncDataFirebaseJobService : JobService() {
+
+    @Inject
+    lateinit var repository: ProjectsRepository
+
+    override fun onStartJob(job: JobParameters): Boolean {
+        AndroidInjection.inject(this)
+
+        repository.loadLocalTasks().observeForever { tasks ->
+            if (tasks != null && !tasks.isEmpty()) {
+                for (task in tasks) {
+                    repository.sendTask(task)
+                }
+            }
+        }
+
+        jobFinished(job, true)
+        return false
+    }
+
+    override fun onStopJob(job: JobParameters): Boolean {
+        return false
+    }
+}
