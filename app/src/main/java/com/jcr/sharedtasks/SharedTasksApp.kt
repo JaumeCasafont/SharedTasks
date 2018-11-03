f53815560aee2992e8f044b5754e980bff473aa4
JaumeCasafont <jaucr94@gmail.com>
2018-11-03 13:32:54 +0100

    Application class to Kotlin

---
+++ app/src/main/java/com/jcr/sharedtasks/SharedTasksApp.kt
@@ -0,0 +1,50 @@
+package com.jcr.sharedtasks
+
+import android.app.Activity
+import android.app.Application
+import android.app.Service
+
+import com.firebase.jobdispatcher.Constraint
+import com.firebase.jobdispatcher.Driver
+import com.firebase.jobdispatcher.FirebaseJobDispatcher
+import com.firebase.jobdispatcher.GooglePlayDriver
+import com.firebase.jobdispatcher.Job
+import com.firebase.jobdispatcher.JobTrigger
+import com.firebase.jobdispatcher.Lifetime
+import com.firebase.jobdispatcher.Trigger
+import com.jcr.sharedtasks.di.AppInjector
+import com.jcr.sharedtasks.sync.SyncDataFirebaseJobService
+
+import javax.inject.Inject
+
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import dagger.android.HasServiceInjector
+import timber.log.Timber
+
+class SharedTasksApp : Application(), HasActivityInjector, HasServiceInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    @Inject
+    lateinit var dispatchingServiceInjector: DispatchingAndroidInjector<Service>
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        AppInjector.init(this)
+    }
+
+    override fun activityInjector(): DispatchingAndroidInjector<Activity>? {
+        return dispatchingAndroidInjector
+    }
+
+    override fun serviceInjector(): AndroidInjector<Service>? {
+        return dispatchingServiceInjector
+    }
+
+}
