f90d84dd31dec3183eb1e343f7e35474051b39d9
JaumeCasafont <jaucr94@gmail.com>
2018-11-22 22:14:22 +0100

    binding package to Kotlin

--- app/src/main/java/com/jcr/sharedtasks/binding/BindingAdapters.kt
+++ app/src/main/java/com/jcr/sharedtasks/binding/BindingAdapters.kt
@@ -14,24 +14,18 @@
  * limitations under the License.
  */
 
-package com.jcr.sharedtasks.binding;
+package com.jcr.sharedtasks.binding
 
-
-import androidx.databinding.DataBindingComponent;
-import androidx.fragment.app.Fragment;
+import androidx.databinding.BindingAdapter
+import android.view.View
 
 /**
- * A Data Binding Component implementation for fragments.
+ * Data Binding adapters specific to the app.
  */
-public class FragmentDataBindingComponent implements DataBindingComponent {
-    private final FragmentBindingAdapters adapter;
-
-    public FragmentDataBindingComponent(Fragment fragment) {
-        this.adapter = new FragmentBindingAdapters(fragment);
-    }
-
-    @Override
-    public FragmentBindingAdapters getFragmentBindingAdapters() {
-        return adapter;
+object BindingAdapters {
+    @JvmStatic
+    @BindingAdapter("visibleGone")
+    fun showHide(view: View, show: Boolean) {
+        view.visibility = if (show) View.VISIBLE else View.GONE
     }
 }
